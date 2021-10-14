f1d20135a53d959def77da036f6e1e565305e100
1SerhiiQA <1serhiiqa@gmail.com>
2021-10-14 21:00:30 +0300

    Hometask 4 is in progress

--- Krasnikov.ITLeadersCourse/ConsoleApp.CSharpBasics/Program.cs
+++ Krasnikov.ITLeadersCourse/ConsoleApp.CSharpBasics/Program.cs
@@ -1,21 +1,15 @@
-﻿using System;
-
-namespace ConsoleApp.CSharpBasics
+﻿namespace ConsoleApp.CSharpBasics
 {
+
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int[] integersArray = { 1, 2, 3, 4, 5 };
-
-            Console.WriteLine(integersArray[4]);
-            Console.WriteLine(integersArray[3]);
-            Console.WriteLine(integersArray[2]);
-            Console.WriteLine(integersArray[1]);
-            Console.WriteLine(integersArray[0]);
-
 
         }
     }
 }
+
+
+
 
