2921a41c484a57e52f957149b55fdcdc76c26e82
1SerhiiQA <1serhiiqa@gmail.com>
2021-11-19 20:37:47 +0200

    Lesson 8 HomeTask in progress

---
+++ Krasnikov.ITLeadersCourse/ConsoleAppWebDriver/Appendix/HomeTasks/Lesson8_ComplexActions/MySelectablePage.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+
+namespace ConsoleApp.WebDriver.Appendix.HomeTasks.Lesson8_ComplexActions
+{
+    public class MySelectablePage : BasePage
+    {
+
+        private IWebElement DemoFrame => FindElement(By.ClassName("demo-frame"));
+
+        private IWebElement Item1Element => FindElement(By.CssSelector("ol#Item 1"));
+        private IWebElement Item3Element => FindElement(By.CssSelector("ol#Item 3"));
+
+        private IWebElement Item5Element => FindElement(By.CssSelector("ol#Item 5"));
+
+        private IWebElement Item7Element => FindElement(By.CssSelector("ol#Item 7"));
+
+        private IWebElement MessageElement => FindElement(By.Id("selectable"));
+
+        public MySelectablePage SwitchToDemoFrame()
+        {
+            Driver.SwitchTo().Frame(DemoFrame);
+
+            return this;
+        }
+
+        public MySelectablePage ClickEveryOddOption()
+        {
+            new Actions(Driver)
+                .Click(Item1Element)
+                .Click(Item3Element)
+                .Click(Item5Element)
+                .Click(Item7Element)
+                .Build()
+                .Perform();
+
+            return this;
+        }
+
+        public string GetMessage() => MessageElement.Text;
+
+        public MySelectablePage SwitchToSelf()
+        {
+            Driver.SwitchTo().DefaultContent();
+
+            return this;
+        }
+    }
+}
--- Krasnikov.ITLeadersCourse/ConsoleAppWebDriver/Program.cs
+++ Krasnikov.ITLeadersCourse/ConsoleAppWebDriver/Program.cs
@@ -1,58 +1,33 @@
-﻿using ConsoleApp.WebDriver.Appendix.HomeTasks.Lesson6_AdditionalWindows;
-using ConsoleApp.WebDriver.Helpers;
-using System;
-using static ConsoleApp.CSharpBasics.IO.Output;
+﻿using System;
+
 
 namespace ConsoleApp.WebDriver
 {
-
     class Program : BaseTest
     {
+        private static string url = AppDomain.CurrentDomain.BaseDirectory + AppSettings.Urls.Url3Lesson8;
 
         static void Main(string[] args)
         {
-
-
-
             try
             {
-                var additionalWindowPage = NavigateTo<AdditionalWindowPage>(AppDomain.CurrentDomain.BaseDirectory + Settings.Urls.UrlLesson6);
-                var additionalWindowPageHandle = WindowHelper.AddWindow();
-
-                var automationPracticePage = MessageWindowPage.ClickNewMessageWindowLink();
-                //var automationPracticePage = AdditionalWindowPage.ClickNewMessageWindowLink();
-                var automationPracticePageHandle = WindowHelper.AddWindow();
-                WindowHelper.SwitchTo(automationPracticePageHandle);
 
 
 
-                //var firstWindowPage = NavigateTo<FirstWindowPage>(AppDomain.CurrentDomain.BaseDirectory + Settings.Urls.UrlLesson6);
-                //var secondWindowPage = firstWindowPage.ClickSecondPageLink();
-                //var thirdWindowPage = secondWindowPage.ClickThirdPageLink();
-                //var message = thirdWindowPage.GetMessage();
 
-                //Out.WriteLine($"Message: {message}");
-
-                var message = NewBrowserTabText.GetNewBrowserTabText();
-
-                Out.WriteLine($"Message: {message}");
-            }
-            catch (Exception)
+                catch (Exception)
             {
             }
             finally
             {
                 DisposeTest();
             }
+
+
+
         }
-
     }
-
-
-
-
 }
-
 
 
 
